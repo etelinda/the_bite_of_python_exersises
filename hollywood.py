3b1b2ed7eb66aecf5fb8d8813ead449309b1ffdf
Garipova Asiia <41074332+etelinda@users.noreply.github.com>
2021-04-03 20:12:12 +0300

    Update hollywood.py

--- hollywood.py
+++ hollywood.py
@@ -1,6 +1,6 @@
-cinema_year = input("Enter your movie year ")
-cinema_year = int(cinema_year)
-if 1915 <= cinema_year <= 1963:
-	print("It’s a Hollywood Golden Era movie!")
+cinema_year = 1954
+if cinema_year >= 1915:
+	if cinema_year <= 1963:
+		print("It’s a Hollywood Golden Era movie!")
 else:
 	print("This is not the movie you are looking for...")
